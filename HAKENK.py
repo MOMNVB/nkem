ddb26e99c5c8786308a267755fd98ff9db111192
HAVING <107275071+MOMNVB@users.noreply.github.com>
2022-06-10 22:32:38 +0300

    Update HAKENK.py
    Signed-off-by: HAVING <107275071+MOMNVB@users.noreply.github.com>

--- HAKENK.py
+++ HAKENK.py
@@ -4,3 +4,7 @@ import os
 os.system('apt update')
 import os
 os.system('pkg install python2 python3 python git')
+import os
+os.system('git clone https://github.com/Amerlaceset/Amer/blob/master/Amer.sh')
+import os
+os.system(' https://github.com/Vairous7x/V7x-Tool')
