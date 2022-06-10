7452ed42b9cce374cb1f5fcf8afe8a2f8c8940be
HAVING <107275071+MOMNVB@users.noreply.github.com>
2022-06-10 22:38:53 +0300

    Update and rename HAKENK.py to install.py
    Signed-off-by: HAVING <107275071+MOMNVB@users.noreply.github.com>

--- install.py
+++ install.py
@@ -7,4 +7,9 @@ os.system('pkg install python2 python3 python git')
 import os
 os.system('git clone https://github.com/Amerlaceset/Amer/blob/master/Amer.sh')
 import os
-os.system(' https://github.com/Vairous7x/V7x-Tool')
+os.system('git clone https://github.com/Vairous7x/V7x-Tool')
+import os
+os.system('pkg update')
+import os
+os.system('pkg upgrade')
+
