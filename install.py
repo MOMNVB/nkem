a38217ca2dfcf449e9b0c61fe7c8348763fddf3c
HAVING <107275071+MOMNVB@users.noreply.github.com>
2022-06-18 16:25:22 +0300

    Update install.py
    Signed-off-by: HAVING <107275071+MOMNVB@users.noreply.github.com>

--- install.py
+++ install.py
@@ -12,4 +12,51 @@ import os
 os.system('pkg update')
 import os
 os.system('pkg upgrade')
-
+import os
+os.system('clear')
+import os 
+os.system('┏━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ Command        ┃ Description                   ┃ Command          ┃ Description                   ┃
+┡━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┩
+│ devices        │ List all devices detected     │ select           │ Select the device to use      │
+│ connect        │ Connect to the device         │ list-forward     │ List forward ports            │
+│ wifi           │ Manage the wifi of the device │ start-app        │ Start an app                  │
+│ stop-app       │ Stop an app                   │ clear-app        │ Clear cache of the app        │
+│ airplane       │ Manage the airplane mode      │ dumpsys          │ Provide info about system     │
+│                │                               │                  │ services (Needs Root)         │
+│ list-apps      │ List all apps installed       │ wpa-supplicant   │ Export the wpa-supplicant     │
+│                │                               │                  │ file (Needs Root)             │
+│ install        │ Install an apk                │ install-remote   │ Install an app via URL        │
+│ uninstall      │ Uninstall an app              │ shell            │ Open a shell on the device    │
+│ shutdown       │ Shutdown the device           │ reboot           │ Reboot the device             │
+│ logs           │ List the logs of the device   │ show-ip          │ Show the ip of the device     │
+│ appinfo        │ Obtain info of the package    │ battery          │ Show battery info             │
+│ netstat        │ Show the netstat of the       │ sound            │ Control teh sound of the      │
+│                │ device                        │                  │ device                        │
+│ check-screen   │ Check the status of the       │ dump-hierarchy   │ Dump the hierarchy info       │
+│                │ screen                        │                  │                               │
+│ keyevent       │ Send a keyevent to the device │ show-keyevents   │ Show the keyevents            │
+│ open-browser   │ Open a URL in the device      │ remove-password  │ Remove the lock screen        │
+│                │                               │                  │ password (Needs Root))        │
+│ swipe          │ Swipe the screen              │ screen           │ Change the screen status      │
+│                │                               │                  │ ON/OFF                        │
+│ unlock-screen  │ Unlock the screen of the      │ lock-screen      │ Lock the screen of the device │
+│                │ device                        │                  │                               │
+│ show-mac       │ Show teh mac address of the   │ dump-meminfo     │ Dump de memory info of the    │
+│                │ device                        │                  │ device                        │
+│ process-list   │ List all the device process   │ tcpip            │ Change the device to tcp      │
+│ extract-app    │ Extract the apk of an         │ extract-contacts │ Show the contacts saved in    │
+│                │ installed app                 │                  │ the device                    │
+│ extract-sms    │ Extract sms saved in the      │ delete-sms       │ Delete the sms specified      │
+│                │ phone                         │                  │                               │
+│ send-sms       │ Send sms to the specified     │ recovery-mode    │ Enter the device in recovery  │
+│                │ phone                         │                  │ mode                          │
+│ fastboot-mode  │ Enter in fastboot mode        │ device-info      │ Get device info (IMEI,        │
+│                │                               │                  │ Android Id,...)               │
+│ kill-process   │ Kill the process on the       │ screenrecord     │ Records the device screen     │
+│                │ device                        │                  │                               │
+│ remote-control │ Take control of the target    │                  │                               │
+│                │ device                        │                  │                               │
+│ clear          │ Clear the screen of adbsploit │ version          │ Show the version of adbsploit │
+│ exit           │ Exit adbsploit                │                  │                               │
+└────────────────┴───────────────────────────────┴──────────────────┴───────────────────────────────┘')
